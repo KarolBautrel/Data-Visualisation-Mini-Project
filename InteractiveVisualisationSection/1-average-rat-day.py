1c938993429eafd0bee6200dbace03be2e7ab474
KarolBautrel <93617105+KarolBautrel@users.noreply.github.com>
2021-12-02 01:59:06 +0100

    Update 1-average-rat-day.py

--- InteractiveVisualisationSection/1-average-rat-day.py
+++ InteractiveVisualisationSection/1-average-rat-day.py
@@ -2,7 +2,7 @@ import pandas
 from datetime import datetime
 from pytz import utc
 import justpy as jp
-# wrzucamy kod uzyskany z Highchart do zmiennej w formie stringa
+
 chart_def = """
 {
     chart: {
@@ -69,32 +69,20 @@ day_average = data.groupby(["Day"]).mean()
 
 
 def app():
-    # Zainicjowanie strony w frameworku Quasar # main component
+  
     wp = jp.QuasarPage()
-    # Nagłówek (a = do jakiej strony)
+
     h1 = jp.QDiv(a=wp, text="Analysis of Course Reviews",
                  classes='''text-h2  text-center q-pt-md
-''')  # heading 1
-    # pointer1
+''') 
     p1 = jp.QDiv(a=wp, text="These graphs represent course review analysis:",
                  classes="""text-h5 q-pl-lg  q-pt-lg""")
     hc = jp.HighCharts(
-        a=wp, options=chart_def)  # options to przypisanie stringa z Highchart
-    # Hierarchicznie mozemy dochodzic do zawartosci stringa z jsa, poniewaz jest on podobny do dict
-    #hc.options.title.text = "Average rating by Day"
-    #hc.options.subtitle.text = " By whole courser "
-    #hc.options.xAxis.title.text = "Ratings"
-    #hc.options.yAxis.title.text = "Dates"
-   # hc.options.tooltip.pointFormat = "{point.y}"
-    # z racji tego, ze oryginalnie day_average_index nie jest czytany przez hc jako liczby, to musimy stworzyc nowy klucz w
-    # xAxis i dodac do niego day_average.index przekonwertowany na liste
-   # hc.options.xAxis.categories = list(day_average.index)
-    # zmiana daty, series[0], poniewaz series to lista w ktorej jest dictionary
-   # hc.options.series[0].name = "Rating"
+        a=wp, options=chart_def)  
     hc.options.series[0].data = list(
         list(day_average["Rating"]))
     return wp
 
 
-# funkcja odpowiadajaca za wygenerowanie strony, jako argument przyjmuje funkcje, która buduje strone
+
 jp.justpy(app)
